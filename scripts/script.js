41d7ed0bd7bc417a719454e03444a14c182ee836
Saul <sauloteromelchor@gmail.com>
2023-02-23 10:13:53 +0100

    Primera subida

---
+++ scripts/script.js
@@ -0,0 +1,66 @@
+window.onload = function (){
+    let divcontenido=document.querySelector(".contenido");
+
+    async function cargaJuegos(){
+        const response= await fetch("./scripts/juegos.json");
+        const juegos=await response.json();
+
+        imprimir(juegos)
+    }
+
+    function imprimir(juegos){
+
+        
+
+
+        for(let juego of juegos){
+            let divjuego=document.createElement("div");
+            divjuego.className="juego";
+            let divposicion=document.createElement("div");
+            divposicion.className="cartaposicion";
+            let divfront=document.createElement("div");
+            divfront.className="cartafront";
+            let divback=document.createElement("div");
+            divback.className="cartaback";
+            
+            //Creamos lo elementos del front
+            let myimg=document.createElement("img");
+            myimg.src=`./img/${juego.foto}`;
+            divfront.appendChild(myimg);
+
+            let myh1=document.createElement("h1");
+            myh1.textContent=juego.nombre;
+            divfront.appendChild(myh1);
+
+           //Creamos los elementos del back
+            let spnombre=document.createElement("span");
+            spnombre.className="spnombre";
+            spnombre.textContent=juego.nombre;
+            divback.appendChild(spnombre);
+
+            let spgenero=document.createElement("span");
+            spgenero.className="spgenero";
+            spgenero.textContent=`Genero: ${juego.genero}`;
+            divback.appendChild(spgenero);
+
+            let spunidades=document.createElement("span");
+            spunidades.className="spunidades";
+            spunidades.textContent=`Unidades: ${juego.unidades}`;
+            divback.appendChild(spunidades);
+
+            let spprecio=document.createElement("span");
+            spprecio.className="spprecio";
+            spprecio.textContent=juego.precio+juego.moneda;
+            divback.appendChild(spprecio);
+
+            //Apendeamos todo
+            divposicion.appendChild(divfront);
+            divposicion.appendChild(divback);
+            divjuego.appendChild(divposicion);
+            divcontenido.appendChild(divjuego);
+        }
+    }
+
+    cargaJuegos();
+
+}
